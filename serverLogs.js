33c797497819e170e8bb4726a37c5742a8d968d6
Brunno Zonatto <brzonatto@gmail.com>
2025-01-21 20:52:49 -0300

    add dados

--- serverLogs.js
+++ serverLogs.js
@@ -1,34 +1,6 @@
 const db = require('./data/db')
 const rotsApi = require('./data/rotsApi')
 
-// dotenv
-const dotenv = require("dotenv");
-dotenv.config();
-const { CHANNEL_SERVER_LOGS_ID } = process.env;
-
-
-const compareLevel = async () => {
-    const players = await db.findAll()
-    let response = []
-
-    for (const player of players) {
-        let playerApiRots = await rotsApi.findPlayerByID(player.id)
-        let lastLevelSaved = player.level
-        let lastLevelApiRots = playerApiRots.level
-        let updatedPlayer = player
-
-        if (lastLevelApiRots > lastLevelSaved) {
-            updatedPlayer.level = playerApiRots.level
-            updatedPlayer.former_level = lastLevelSaved
-            updatedPlayer.log_type = 'upLevel'
-            response.push(updatedPlayer)
-            //console.log(`mudou o level de ${player.name}`)
-        }
-    }
-
-    return response
-}
-
 const compareDeaths = async () => {
     const localPlayers = await db.findAll(); // Busca todos os jogadores da base local
     const differences = []; // Array para armazenar as diferenças
@@ -36,21 +8,32 @@ const compareDeaths = async () => {
 
     for (const localPlayer of localPlayers) {
         const rotsPlayer = await rotsApi.findPlayerByID(localPlayer.id); // Busca o jogador na API
-        console.log('name', localPlayer.name)
-        console.log('localPlayer', localPlayer.deaths?.deaths[0]?.time)
-        console.log('rotsPlayer', rotsPlayer.deaths?.deaths[0]?.time)
-        console.log('###########################################################')
+        console.log('name', localPlayer.name);
+        console.log('localPlayer', localPlayer.deaths?.deaths?.[0]?.time);
+        console.log('rotsPlayer', rotsPlayer.deaths?.deaths?.[0]?.time);
+        console.log('###########################################################');
 
-        // throw new Error("pera");
+        let playerKilledBy = null;
+        let playerMostDamageBy = null;
 
+        if (rotsPlayer.deaths?.deaths?.[0]?.is_player === 1) {
+            const killedByResponse = await rotsApi.findIDPlayer(rotsPlayer.deaths.deaths[0].killed_by);
+            playerKilledBy = Array.isArray(killedByResponse) ? killedByResponse[0] : null;
+        }
 
-        // Compara a "death" da posição 0
-        if (localPlayer.deaths?.deaths[0]?.time !== rotsPlayer.deaths?.deaths[0]?.time) {
+        if (rotsPlayer.deaths?.deaths?.[0]?.mostdamage_is_player === 1) {
+            const mostDamageByResponse = await rotsApi.findIDPlayer(rotsPlayer.deaths.deaths[0].mostdamage_by);
+            playerMostDamageBy = Array.isArray(mostDamageByResponse) ? mostDamageByResponse[0] : null;
+        }
+
+        if (localPlayer.deaths?.deaths?.[0]?.time !== rotsPlayer.deaths?.deaths?.[0]?.time) {
             differences.push({
                 id: localPlayer.id,
                 name: localPlayer.name,
-                pvp_type: localPlayer.pvp_type,             // Adiciona o nome do localPlayer
-                death: rotsPlayer.deaths?.deaths[0],  // Adiciona a diferença de deaths
+                playerKilledByID: playerKilledBy?.id || null,
+                playerMostDamageByID: playerMostDamageBy?.id || null,
+                pvp_type: localPlayer.pvp_type,
+                death: rotsPlayer.deaths?.deaths?.[0] || null,
             });
         }
 
@@ -61,27 +44,6 @@ const compareDeaths = async () => {
     return differences; // Retorna o array com as diferenças
 };
 
-
-// const compareDeath = async () => {
-//     const players = await db.findAll()
-//     let response = []
-
-//     for (const player of players) {
-//         let playerApiRots = await rotsApi.findPlayerByID(player.id)
-//         let lastDeathSaved = player.deaths.deaths[0]
-//         let lastDeathApiRots = playerApiRots.deaths.deaths[0]
-//         let updatedPlayer = player 
-
-//         if (lastDeathSaved.time != lastDeathApiRots.time){
-//             updatedPlayer.deaths = playerApiRots.deaths
-//             updatedPlayer.log_type = 'newDeath'
-//             response.push(updatedPlayer)
-//             //console.log(`mudou a morte de ${player.name}`)
-//         }  
-//     }
-
-//     return response  
-// }
 
 module.exports = {
     compareDeaths
